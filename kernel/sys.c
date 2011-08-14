7be6e8bf7fdb4fbb6be4ad9d4aecf1e87c7e95a7
Frederic Weisbecker <fweisbec@gmail.com>
2011-08-15 00:20:23 +0200

    nohz/cpuset: Flush cputimes for getrusage() and times() syscalls
    Both syscalls need to iterate through the thread group to get
    the cputimes. As some threads of the group may be running on
    nohz cpuset, we need to flush the cputimes there.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Anton Blanchard <anton@au1.ibm.com>
    Cc: Avi Kivity <avi@redhat.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Lai Jiangshan <laijs@cn.fujitsu.com>
    Cc: Paul E . McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Paul Menage <menage@google.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Stephen Hemminger <shemminger@vyatta.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tim Pepper <lnxninja@linux.vnet.ibm.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -43,6 +43,7 @@
 #include <linux/syscalls.h>
 #include <linux/kprobes.h>
 #include <linux/user_namespace.h>
+#include <linux/cpuset.h>
 
 #include <linux/kmsg_dump.h>
 
@@ -908,6 +909,8 @@ void do_sys_times(struct tms *tms)
 {
 	cputime_t tgutime, tgstime, cutime, cstime;
 
+	cpuset_nohz_flush_cputimes();
+
 	spin_lock_irq(&current->sighand->siglock);
 	thread_group_times(current, &tgutime, &tgstime);
 	cutime = current->signal->cutime;
@@ -1535,6 +1538,9 @@ static void k_getrusage(struct task_struct *p, int who, struct rusage *r)
 		maxrss = p->signal->maxrss;
 		goto out;
 	}
+
+	/* For thread_group_times */
+	cpuset_nohz_flush_cputimes();
 
 	if (!lock_task_sighand(p, &flags))
 		return;
