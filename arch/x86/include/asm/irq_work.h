cb5fac0de0e885e004817a5bd164be91f3748c7d
Frederic Weisbecker <fweisbec@gmail.com>
2012-10-12 13:59:35 +0200

    x86: Implement arch_irq_work_use_tick
    Most of the time, x86 can trigger self-IPIs. Tell
    irq work subsystem about it.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>

--- arch/x86/include/asm/irq_work.h
+++ arch/x86/include/asm/irq_work.h
@@ -2,8 +2,12 @@
 #define _ASM_X86_IRQ_WORK_H
 
 #ifdef CONFIG_X86_LOCAL_APIC
+#include <asm/cpufeature.h>
+
 extern void __arch_irq_work_raise(void);
 #define arch_irq_work_raise __arch_irq_work_raise
+
+#define arch_irq_work_use_tick() (!cpu_has_apic)
 #endif
 
 #include <asm-generic/irq_work.h>
